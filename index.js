8e0cd4c56a725b0e73dbd0eb65d285495f397f58
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-05-12 21:51:12 -0500

    added css and generatable html

--- index.js
+++ index.js
@@ -13,28 +13,28 @@ function managerQuestions() {
     inquirer.prompt([
     {
         type: 'input',
-        name: 'manager-name',
+        name: 'name',
         message: "What is the manager's name?"
     },
     {
         type: "input",
-        name: 'manager-id',
+        name: 'id',
         message: "What is the managers employee ID number?"
     },
     {
         type: "input",
-        name: "manager-email",
+        name: "email",
         message: "What is the manager's email address?"
     },
     {
         type: "input",
-        name: "office-number",
+        name: "officeNumber",
         message: "What is the office number?"
     }
 ])
 .then(function(data){
-    const email = data.email;
-    const name = data.name;
+    const email = data.email
+    const name = data.name
     const officeNumber = data.officeNumber
     const id = data.id
     const newMember = new Manager(name, id, email, officeNumber)
@@ -74,22 +74,22 @@ function engineerQuestions() {
     inquirer.prompt([
     {
         type: "input",
-        name: "engineer-name",
+        name: "name",
         message: "What is the engineer's name?"
     },
     {
         type: "input",
-        name: "engineer-id",
+        name: "id",
         message: "What is the engineer's ID number?"
     },
     {
         type: "input",
-        name: "engineer-email",
+        name: "email",
         message: "what is the engineer's email address?"
     },
     {
         type: "input",
-        name: "engineer-github",
+        name: "gitHub",
         message: "What is the engineer's Github username?"
     },
 ])
@@ -108,17 +108,17 @@ function internQuestions() {
     inquirer.prompt([
     {
         type: "input",
-        name: "intern-name",
+        name: "name",
         message: "What is the intern's name?"
     },
     {
         type: "input",
-        name: "intern-id",
+        name: "id",
         message: "What is the intern's ID number?"
     },
     {
         type: "input",
-        name: "intern-email",
+        name: "email",
         message: "What is the intern's email address?"
     },
     {
@@ -137,11 +137,70 @@ function internQuestions() {
     options();
 })
 }
- 
+
+ const htmlArr = [];
 function formTeam() {
+const htmlTop =`
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../htmlTest/style.css">
+    <title>Work Team</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="header">
+            <h1>Team Members</h1>
+        </header>
+        `
+        htmlArr.push(htmlTop)
+            for (var i = 0; i< workTeam.length; i++){
+let htmlBottom = `
+            
+        <div class="row">
+            <div class="column">
+                <div class="test-card">
+                    <h1>${workTeam[i].title}</h1>
+                    <p>Team member's Name: ${workTeam[i].name}</p>
+                    <p>Team member's ID: ${workTeam[i].id}</p>
+                    <p>Email: <a href = "mailto: ${workTeam[i].email}">${workTeam[i].email}</a></p>
+                    `
+                    if(workTeam[i].gitHub){
+                        htmlBottom += `
+                        <p>GitHub link: <a href="https://github.com/${workTeam[i].gitHub}">${workTeam[i].gitHub}</a></p> 
+                        `
+                    }
+                    if(workTeam[i].school){
+                        htmlBottom += `
+                        <p>School attended: ${workTeam[i].school}</p> 
+                        `
+                    }
+                    if(workTeam[i].officeNumber){
+                        htmlBottom += `
+                        <p>Officenumber: #${workTeam[i].officeNumber}</p> 
+                        `
+                    }
+                    htmlArr.push(htmlBottom)
+                    const htmlEnd = `
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+`
+htmlArr.push(htmlEnd)
+
 
 }
+fs.writeFile(`./generate-html/${workTeam[0]}.html`, htmlArr.join(""), function(err){
+    console.log(err)
+    })
+}
 managerQuestions();
-
-
-
